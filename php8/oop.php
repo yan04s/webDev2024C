1ef5a02bf5dd699bd83fa4cb689e1fc9594625f8
yan04s <130055001+yan04s@users.noreply.github.com>
2024-11-05 16:37:38 +0800

    s

--- php8/oop.php
+++ php8/oop.php
@@ -3,11 +3,11 @@ class car{//parent class
     public $model="";
     public function setModel($newVal){
         $this->model = $newVal;
-    }
+    };
     public function getModel(){
         return "The model is ".$this->model."<br>";
-    }
-}
+    };
+};
 
 class compact extends car{//child class
     private $seat=0;
@@ -17,11 +17,11 @@ class compact extends car{//child class
     public function getSeat(){
         return "Number of seats is ".$this->seat."<br>";
     };
-}
+};
 
-$car1 = new compact;
+$car1 = new compact();
 $car1->setModel("Toyota");
 echo $car1->getModel();
-$car1->setSeat(5);
+$car1->setSeat(4);
 echo $car1->getSeat();
 ?>
