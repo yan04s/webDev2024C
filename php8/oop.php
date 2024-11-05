7e4205624cd1926d760c3d55ebdbbef700b6c2f5
yan04s <130055001+yan04s@users.noreply.github.com>
2024-11-05 16:37:58 +0800

    s

--- php8/oop.php
+++ php8/oop.php
@@ -22,6 +22,10 @@ class compact extends car{//child class
 $car1 = new compact();
 $car1->setModel("Toyota");
 echo $car1->getModel();
-$car1->setSeat(4);
+$car1->setSeat(5);
 echo $car1->getSeat();
+
+$car2 = new compact();
+$car2->setModel("Honda");
+echo $car2->getModel();
 ?>
