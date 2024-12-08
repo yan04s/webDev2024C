c7944928ba5b3ea5df3704c34d5f505729c23f44
yan04s <130055001+yan04s@users.noreply.github.com>
2024-12-08 21:57:05 +0800

    j

--- php8/oop.php
+++ php8/oop.php
@@ -34,12 +34,12 @@ class mpv extends car{//child class
 $car1 = new compact();
 $car1->setModel("Toyota");
 echo $car1->getModel();
-$car1->setSeat(5,6);
+$car1->setSeat(5);
 echo $car1->getSeat();
 
 $car2 = new mpv();
 $car2->setModel("Honda");
 echo $car2->getModel();
-$car2->setSeat(5,7);
+$car2->setSeat(5);
 echo $car2->getSeat();
 ?>
